a48389ceedd37c06408d8df73f045232b4305877
jakubklapka <klapka.jacob@icloud.com>
2021-11-02 15:37:56 +0100

    sd

--- components/editor/script.js
+++ components/editor/script.js
@@ -3,13 +3,9 @@ function editorDownload(textToWrite, fileNameToSaveAs) {
   var downloadLink = document.createElement("a");
   downloadLink.download = fileNameToSaveAs;
   downloadLink.innerHTML = "Download File";
-  if (window.webkitURL != null) {
-    // Chrome allows the link to be clicked
-    // without actually adding it to the DOM.
+  if (window.webkitURL != null) { // Chrome
     downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-  } else {
-    // Firefox requires the link to be added to the DOM
-    // before it can be clicked.
+  } else { // Firefox
     downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
     downloadLink.onclick = destroyClickedElement;
     downloadLink.style.display = "none";
@@ -19,23 +15,48 @@ function editorDownload(textToWrite, fileNameToSaveAs) {
   downloadLink.click();
 }
 
-async function editorInsertFormatting(txtarea, text) {
-    var selectStart = txtarea.selectionStart
-    var selectEnd = txtarea.selectionEnd
-    var scrollPos = txtarea.scrollTop;
-    var caretPos = txtarea.selectionStart;
+//
 
-    var front = (txtarea.value).substring(0, caretPos);
-    var back = (txtarea.value).substring(txtarea.selectionEnd, txtarea.value.length);
-    var middle = (txtarea.value).substring(caretPos, txtarea.selectionEnd);
-    txtarea.value = front + text + middle + text + back;
-    if (selectStart !== selectEnd) {
-        txtarea.selectionStart = selectStart + text.length
-        txtarea.selectionEnd = selectEnd + text.length
-    } else {
-        txtarea.selectionStart = selectStart + text.length
-        txtarea.selectionEnd = txtarea.selectionStart
-    }
-    txtarea.focus();
-    txtarea.scrollTop = scrollPos;
+async function editorInsertFormatting(txtarea, text) {
+  var selectStart = txtarea.selectionStart; // "Select" the selection
+  var selectEnd = txtarea.selectionEnd;
+  var scrollPos = txtarea.scrollTop;
+  var caretPos = txtarea.selectionStart;
+
+  var front = txtarea.value.substring(0, caretPos);
+  var back = txtarea.value.substring(
+    txtarea.selectionEnd,
+    txtarea.value.length
+  );
+  var middle = txtarea.value.substring(caretPos, txtarea.selectionEnd);
+  txtarea.value = front + text + middle + text + back;
+  if (selectStart !== selectEnd) {
+    txtarea.selectionStart = selectStart + text.length;
+    txtarea.selectionEnd = selectEnd + text.length;
+  } else {
+    txtarea.selectionStart = selectStart + text.length;
+    txtarea.selectionEnd = txtarea.selectionStart;
+  }
+  txtarea.focus();
+  txtarea.scrollTop = scrollPos;
+}
+
+function parseMarkdown(markdownText) {
+	const htmlText = markdownText
+		.replace(/^### (.*$)/gim, '<h3>$1</h3>')
+		.replace(/^## (.*$)/gim, '<h2>$1</h2>')
+		.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+		.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+		.replace(/\*(.*)\*/gim, '<i>$1</i>')
+		.replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+		.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+		.replace(/\n$/gim, '<br>')
+
+	return htmlText.trim()
+}
+
+function liveParser(){
+  const text = document.getElementById("text").value
+  console.log(parseMarkdown(text))
 }
