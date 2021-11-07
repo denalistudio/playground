875c5589491f8ccb928f0eb5dda662b678f3ecaa
jakubklapka <klapka.jacob@icloud.com>
2021-11-07 22:29:19 +0100

    sd

--- docs/editor/script.js
+++ docs/editor/script.js
@@ -1,20 +1,25 @@
-function editorDownload(textToWrite, fileNameToSaveAs) {
-  var textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
-  var downloadLink = document.createElement("a");
-  downloadLink.download = fileNameToSaveAs;
-  downloadLink.innerHTML = "Download File";
-  if (window.webkitURL != null) {
-    // Chrome
-    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-  } else {
-    // Firefox
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-  }
+function editorParseMarkdown(markdownText) {
+  const htmlText = markdownText
+    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
+    .replace(/\*(.*)\*/gim, "<i>$1</i>")
+    .replace(/\~\~(.*)\~\~/gim, "<s>$1</s>")
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+    .replace(/^#### (.*$)/gim, "<h4>$1</h4>")
+    .replace(/^##### (.*$)/gim, "<h5>$1</h5>")
+    .replace(/^###### (.*$)/gim, "<h6>$1</h6>")
+    .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
+    .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+    .replace(/\n$/gim, "<br>");
+  return htmlText.trim();
+}
 
-  downloadLink.click();
+function editorLiveParser() {
+  const text = document.getElementById("text").value;
+  var htmltext = editorParseMarkdown(text);
+  document.getElementById("parsed").innerHTML = htmltext;
 }
 
 function editorInsertFormatting(txtarea, text) {
@@ -39,46 +44,54 @@ function editorInsertFormatting(txtarea, text) {
   }
   txtarea.focus();
   txtarea.scrollTop = scrollPos;
+  editorLiveParser()
 }
 
-function parseMarkdown(markdownText) {
-  const htmlText = markdownText
-    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
-    .replace(/\*(.*)\*/gim, "<i>$1</i>")
-    .replace(/\~\~(.*)\~\~/gim, "<s>$1</s>")
-    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
-    .replace(/^#### (.*$)/gim, "<h4>$1</h4>")
-    .replace(/^##### (.*$)/gim, "<h5>$1</h5>")
-    .replace(/^###### (.*$)/gim, "<h6>$1</h6>")
-    .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
-    .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
-    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-    .replace(/\n$/gim, "<br>");
-  return htmlText.trim();
+function editorDownload(textToWrite, fileNameToSaveAs) {
+  var textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
 }
 
-function liveParser() {
-  const text = document.getElementById("text").value;
-  var htmltext = parseMarkdown(text);
-  document.getElementById("parsed").innerHTML = htmltext;
-}
-
-function liveParserOutput() {
-  const text = document.getElementById("text").value;
-  var htmltext = parseMarkdown(text);
-  console.log("liveParser output: " + htmltext);
-}
-
-function devtoolsOpen() {
+function editorDevtoolsOpen() {
   document.getElementById("devtools").style.display = "block";
 }
 
-function devtoolsClose() {
+function editorDevtoolsClose() {
   document.getElementById("devtools").style.display = "none";
 }
 
-function printTestText() {
-  document.getElementById("text").value = "# Test text";
+function editorDevtoolsConsoleOutput() {
+  const text = document.getElementById("text").value;
+  var htmltext = editorParseMarkdown(text);
+  console.log("liveParser output: " + htmltext);
+}
+
+function editorDevtoolsTestText() {
+  let input = document.getElementById("text");
+
+  // load default.txt into input box
+  try {
+    let fileToLoad = "default.txt";
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", fileToLoad, false);
+    xmlhttp.send();
+    input.innerHTML = xmlhttp.responseText;
+  } catch (DOMException) {
+    input.innerHTML = "Error loading file. Maybe related to filepath or CORS?";
+  }
+  editorLiveParser()
 }
