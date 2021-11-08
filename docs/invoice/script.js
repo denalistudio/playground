3b33f9ad713c1d41f1400d1e4c08b37608756182
jakubklapka <klapka.jacob@icloud.com>
2021-11-08 20:47:51 +0100

    invoice print

--- docs/invoice/script.js
+++ docs/invoice/script.js
@@ -1,3 +1,14 @@
 function invoiceidWrite() {
     document.getElementById('invoicevariable-invoiceid').innerHTML = document.getElementById('inputInvoiceID').value;
 }
+
+function printDiv() {
+    var divContents = document.getElementById("paper").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<body > <h1>Div contents are <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
