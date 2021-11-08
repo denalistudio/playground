f1da2dda0cfae86fa308b6f37a70b6a609f20e66
jakubklapka <klapka.jacob@icloud.com>
2021-11-08 20:49:44 +0100

    sd

--- docs/invoice/script.js
+++ docs/invoice/script.js
@@ -6,7 +6,7 @@ function printDiv() {
     var divContents = document.getElementById("paper").innerHTML;
     var a = window.open('', '', 'height=500, width=500');
     a.document.write('<html>');
-    a.document.write('<body > <h1>Div contents are <br>');
+    a.document.write('<body>');
     a.document.write(divContents);
     a.document.write('</body></html>');
     a.document.close();
