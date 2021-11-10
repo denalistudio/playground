775a6efce174cb4922d59034295f441162b9f573
jakubklapka <klapka.jacob@icloud.com>
2021-11-10 18:58:05 +0100

    update docs/invoice/index.html, script.js, style.css and _invoice.scss

--- docs/invoice/script.js
+++ docs/invoice/script.js
@@ -1,18 +1,26 @@
-function invoiceidWrite() {
-    document.getElementById('invoicevariable-invoiceid').innerHTML = document.getElementById('invoice-input-invoicenumber').value;
+function invoiceWrite(variable, input) {
+  document.getElementById(variable).innerHTML =
+    document.getElementById(input).value;
 }
 
-function invoicePurchaserNameWrite() {
-    document.getElementById('invoice-variable-purchasername').innerHTML = document.getElementById('invoice-input-purchasername').value;
+function invoiceNumber() {
+  var a = new Date();
+  var month = ("0" + (a.getMonth() + 1)).slice(-2);
+  var year = ("0" + a.getFullYear()).slice(-2);
+  console.log(month + year);
+  var myNumber = document.getElementById('number-input').value;
+  var formattedNumber = ("0000" + myNumber).slice(-4);
+    var invoiceNumber = month + year + formattedNumber;
+  document.getElementById('number-variable').innerHTML = invoiceNumber
 }
 
 function printDiv() {
-    var divContents = document.getElementById("paper").innerHTML;
-    var a = window.open('', '', 'height=500, width=500');
-    a.document.write('<html>');
-    a.document.write('<body>');
-    a.document.write(divContents);
-    a.document.write('</body></html>');
-    a.document.close();
-    a.print();
+  var divContents = document.getElementById("paper").innerHTML;
+  var a = window.open("", "", "height=500, width=500");
+  a.document.write("<html>");
+  a.document.write("<body>");
+  a.document.write(divContents);
+  a.document.write("</body></html>");
+  a.document.close();
+  a.print();
 }
