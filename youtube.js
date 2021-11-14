4691d2b4af6cb5038a3edeec9df9fbc084f13fb8
jakubklapka <klapka.jacob@icloud.com>
2021-11-14 11:27:38 +0100

    update 1 file and delete 1 file

--- youtube.js
+++
@@ -1,6 +0,0 @@
-function YouTubeGetID(url){
-    url = url.split(/(vi\/|v%3D|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-    return undefined !== url[2]?url[2].split(/[^0-9a-z_\-]/i)[0]:url[0];
-}
-
-console.log(ytid);
