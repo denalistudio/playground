be4522401c7934ed92b46223d27724980568d725
jakubklapka <klapka.jacob@icloud.com>
2021-12-11 22:29:57 +0100

    delete 7 files and update 4 files

--- docs/editor/script.js
+++ docs/editor/script.js
@@ -1,74 +1,21 @@
-function format(value){
-    document.execCommand(value,false,'')
+function format(command, value) {
+    document.execCommand(command, false, value);
 }
 
-
-var btn = document.querySelector(".sai");
-var getText = document.querySelector(".getText");
-var content = document.querySelector(".getcontent");
-var editorContent = document.querySelector(".editor");
-
-btn.addEventListener("click", function() {
-  var s = editorContent.innerHTML;
-  content.style.display = "block";
-  content.textContent = s;
-});
-
-getText.addEventListener("click", function() {
-  const old = editorContent.textContent;
-  content.style.display = "block";
-  content.textContent = old;
-});
-
-function link() {
-  var url = prompt("Enter the URL");
-  document.execCommand("createLink", false, url);
+function get_text(el) {
+    ret = "";
+    var length = el.childNodes.length;
+    for (var i = 0; i < length; i++) {
+        var node = el.childNodes[i];
+        if (node.nodeType != 8) {
+            ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
+        }
+    }
+    var ret
 }
 
-function copy() {
-  document.execCommand("copy", false, "");
-}
-
-function changeColor() {
-  var color = prompt("Enter your color in hex ex:#f1f233");
-  document.execCommand("foreColor", false, color);
-}
-
-
-function getImage() {
-  var file = document.querySelector("input[type=file]").files[0];
-
-  var reader = new FileReader();
-
-  let dataURI;
-
-  reader.addEventListener(
-    "load",
-    function() {
-      dataURI = reader.result;
-
-      const img = document.createElement("img");
-      img.src = dataURI;
-      editorContent.appendChild(img);
-    },
-    false
-  );
-
-  if (file) {
-    console.log("s");
-    reader.readAsDataURL(file);
-  }
-}
-
-function printMe() {
-  if (confirm("Check your Content before print")) {
-    const body = document.body;
-    let s = body.innerHTML;
-    body.textContent = editorContent.innerHTML;
-
-    document.execCommandShowHelp;
-    body.style.whiteSpace = "pre";
-    window.print();
-    location.reload();
-  }
+function countWords() {
+    var words = get_text(document.getElementById('text'));
+    var count = words.split(/\s+/).length;
+    alert(count);
 }
