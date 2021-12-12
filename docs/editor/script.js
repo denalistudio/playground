26478b5c7a9c14798d9a459efa9b55522d937322
jakubklapka <klapka.jacob@icloud.com>
2021-12-12 11:54:07 +0100

    update 4 files and delete 1 file

--- docs/editor/script.js
+++ docs/editor/script.js
@@ -2,20 +2,7 @@ function format(command, value) {
     document.execCommand(command, false, value);
 }
 
-function get_text(el) {
-    ret = "";
-    var length = el.childNodes.length;
-    for (var i = 0; i < length; i++) {
-        var node = el.childNodes[i];
-        if (node.nodeType != 8) {
-            ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
-        }
-    }
-    var ret
-}
-
-function countWords() {
-    var words = get_text(document.getElementById('text'));
-    var count = words.split(/\s+/).length;
-    alert(count);
+function count(target){
+    var count = document.getElementById(target).innerHTML.split(' ').length;
+    document.getElementById('counter').innerHTML = count;
 }
