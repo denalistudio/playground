9409cffe1e579cdb9c80299a9c2ec23c541cc2ad
jakubklapka <klapka.jacob@icloud.com>
2021-12-12 15:23:35 +0100

    docs: update style.css, _website.scss and docs/website/index.html

---
+++ docs/website/script.js
@@ -0,0 +1,6 @@
+function makeEditable() {
+    var editable = document.querySelectorAll('h1, h2, h3, p');
+    for (var i = 0; i < editable.length; i++) {
+        editable[i].contentEditable = 'true';
+    }
+}
