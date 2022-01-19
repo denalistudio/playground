2a0078f49132fe7cad76f7c43b0d71eb6c3b89e7
jakubklapka <klapka.jacob@icloud.com>
2022-01-19 19:57:48 +0100

    update script.js, docs/index.html, style.css and style.scss

--- cookies/script.js
+++ cookies/script.js
@@ -4,9 +4,12 @@ function cookies(kind) {
             var div = document.createElement('div');
             div.id = 'cookies';
             div.innerHTML = 'By continuing to use this website, you agree to our use of cookies. <button id="cookies-ok">OK</button>';
-            div.className = 'border pad';
+            // Style
             div.style.position = 'fixed';
+            div.style.bottom = '40px';
+            div.style.margin = 'auto';
             document.body.appendChild(div);
+            console.log('cookies: The "basic" cookie button kind has been loaded.');
             break;
         default:
             console.log('cookies: The "kind" parameter is not defined correctly.');
