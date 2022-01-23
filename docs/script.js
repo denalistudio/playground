73a5e4e47d9786ec7b2d0920eb8f3ff4d76f5dea
jakubklapka <klapka.jacob@icloud.com>
2022-01-23 19:41:19 +0100

    update 5 files and delete 2 files

--- docs/script.js
+++ docs/script.js
@@ -2,18 +2,30 @@ fetch('./comma.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        // Metadata
+        // --------------------------------------------------
         // Set the document title
         document.title = data.name;
         // Set the document description
-        var description = document.createElement('meta'); 
-        description.setAttribute('name', 'description'); 
-        description.content = data.description; 
+        var description = document.createElement('meta');
+        description.setAttribute('name', 'description');
+        description.content = data.description;
         document.getElementsByTagName('head')[0].appendChild(description);
         // Set the document author
-        var author = document.createElement('meta'); 
-        author.setAttribute('name', 'author'); 
-        author.content = data.author; 
+        var author = document.createElement('meta');
+        author.setAttribute('name', 'author');
+        author.content = data.author;
         document.getElementsByTagName('head')[0].appendChild(author);
+        // Set the document keywords
+        var keywords = document.createElement('meta');
+        keywords.setAttribute('name', 'keywords');
+        keywords.content = data.keywords;
+        document.getElementsByTagName('head')[0].appendChild(keywords);
+        // Set the theme color
+        var themecolor = document.createElement('meta');
+        themecolor.setAttribute('name', 'theme-color');
+        themecolor.content = data.color;
+        document.getElementsByTagName('head')[0].appendChild(themecolor);
     })
 
 function makeEditable() {
