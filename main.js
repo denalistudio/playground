711ff1dd164084f3fd953dfa281429302ff83f78
Jakub Klapka <klapka.jacob@icloud.com>
2022-03-30 06:06:11 +0200

    update index.html and main.js

--- main.js
+++ main.js
@@ -8,6 +8,38 @@ const dom = {
     head: document.getElementsByTagName('head')[0],
 }
 
+function load(id, url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xmlhttp = new XMLHttpRequest();
+    } 
+    else 
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) 
+        {
+           if(xmlhttp.status == 200){
+               document.getElementById(id).innerHTML = xmlhttp.responseText;
+               var allScripts = document.getElementById(id).getElementsByTagName('script');
+               for (var n = 0; n < allScripts .length; n++)
+               {
+                   eval(allScripts [n].innerHTML)
+               }
+           }
+           else {
+               alert('Error');
+           }
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
 fetch('./comma.json')
     .then(response => response.json())
     .then(data => {
