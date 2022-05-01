506385a55263df4672b495c7365614b5ad2fbcd5
Jakub Klapka <klapka.jacob@icloud.com>
2022-05-01 09:56:15 +0200

    update admin/playground/index.html and script.js

--- admin/playground/script.js
+++ admin/playground/script.js
@@ -1,15 +1,30 @@
 const rect = document.getElementById("rect");
 
+const output = {
+    borderRadius: document.getElementById("output-borderRadius"),
+    dimensions: document.getElementById("output-dimensions"),
+}
+
 let input = document.querySelector(".input");
 let result = document.querySelector(".result");
 
 input.addEventListener("input", function () {
-    result.innerText = input.value + "px";
+    output.borderRadius.innerText = input.value + "px";
     rect.style.borderRadius = input.value + "px";
 });
 
 function resizer() {
-    document.getElementById("dimensions").innerHTML = rect.clientWidth + "px" + " x " + rect.clientHeight + "px";
+    output.dimensions.innerHTML = rect.clientWidth + "px" + " x " + rect.clientHeight + "px";
 }
 
 new ResizeObserver(resizer).observe(rect);
+
+let hexColor = document.querySelector(".result")
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", function () {
+          let color = Math.random().toString(16).substring(2, 8);
+          color = "#" + color;
+          hexColor.innerText = color;
+          rect.style.backgroundColor = color;
+});
