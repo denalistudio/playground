51a07e5b1ef72c2b20b45fc254e92cabd874366e
Jakub Klapka <klapka.jacob@icloud.com>
2022-05-08 22:31:47 +0200

    update admin/index.html, script.js, style.css and style.scss

--- admin/script.js
+++ admin/script.js
@@ -57,6 +57,28 @@ const editor = {
         sel.addRange(range0);
         range0.collapse(false);
         document.execCommand('insertText', false, type);
+    },
+    syntax: (text) => {
+        var html = text
+            .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>") // bold
+            .replace(/\*(.*)\*/gim, "<i>$1</i>") // italic
+            .replace(/\_(.*)\_/gim, "<u>$1</u>") // underline
+            .replace(/\-(.*)\-/gim, "<s>$1</s>") // strikethrough
+            .replace(/^# (.*$)/gim, "<h1>$1</h1>") // h1
+            .replace(/^## (.*$)/gim, "<h2>$1</h2>") // h2
+            .replace(/^### (.*$)/gim, "<h3>$1</h3>") // h3
+            .replace(/^#### (.*$)/gim, "<h4>$1</h4>") // h4
+            .replace(/^##### (.*$)/gim, "<h5>$1</h5>") // h5
+            .replace(/^###### (.*$)/gim, "<h6>$1</h6>") // h6
+            .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>") // blockquote
+            .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />") // image
+            .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>") // link
+            .replace(/\n$/gim, "<br>"); // linebreak
+        return html.trim();
+    },
+    output: () => {
+        var input = document.getElementById("editor").innerHTML;
+        document.getElementById("text").innerHTML = editor.syntax(input);
     }
 }
 
