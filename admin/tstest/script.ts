bfb80e128d7285987a1f69095ee3d04025038f77
jakubklapka <klapka@studiodenali.cz>
2022-05-29 13:23:32 +0200

    test: update script.ts

--- admin/tstest/script.ts
+++ admin/tstest/script.ts
@@ -5,3 +5,7 @@ interface abc {
     b: boolean;
     c: number;
 }
+
+interface Window {
+    state: 0 | 1 | 2 | 'e'; // Our window state management system supports four states. 0: closed, 1: loading, 2: loaded, e: error
+}
