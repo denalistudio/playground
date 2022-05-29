3a3ff0bb7e59b614d53f1adbcf206dbb1b81f77e
jakubklapka <klapka@studiodenali.cz>
2022-05-29 20:08:51 +0200

    update 5 files

--- admin/login/script.ts
+++ admin/login/script.ts
@@ -1,22 +1,25 @@
-"use strict";
 const login = {
-    email: document.querySelectorAll("[data-login='email']")[0],
-    password: document.querySelectorAll("[data-login='password']")[0],
+    email: <HTMLInputElement>document.querySelectorAll("[data-login='email']")[0],
+    password: <HTMLInputElement>document.querySelectorAll("[data-login='password']")[0],
     toggle: document.querySelectorAll("[data-login='toggle']")[0],
-};
+}
+
 const regex = {
     email: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
 };
+
 const test = () => { console.log(regex.email.test(login.email.value)); };
+
 const toggle = () => {
     if (login.password.type === 'password') {
         login.password.type = 'text';
         login.toggle.innerHTML = 'visibility_off';
-    }
-    else {
+    } else {
         login.password.type = 'password';
         login.toggle.innerHTML = 'visibility';
     }
 };
+
 login.email.addEventListener('keyup', test);
+
 login.toggle.addEventListener("click", toggle);
