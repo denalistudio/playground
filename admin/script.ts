8d48b53016e4e6f59cacd47555fa1dc803b1aaa9
jakubklapka <klapka@studiodenali.cz>
2022-05-29 20:11:15 +0200

    update script.js, script.js, script.js and script.js

--- admin/script.ts
+++ admin/script.ts
@@ -1,13 +1,3 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const spotlight = {
     element: document.querySelector("[data-comma='spotlight']"),
     show: (option) => {
@@ -21,22 +11,22 @@ const spotlight = {
         }
     }
 };
+
 const search = {
     element: document.querySelector("[data-search='element']"),
     input: document.querySelector("[data-search='input']"),
     results: document.querySelector("[data-search='results']"),
     show: () => {
         if (search.element.style.display === 'none') {
-            search.element.style = 'display: block';
+            search.element.style = 'display: block'
             search.input.value = "";
             spotlight.show('on');
             search.input.focus();
         }
         else {
-            search.element.style = 'display: none';
+            search.element.style = 'display: none'
             spotlight.show('off');
-        }
-        ;
+        };
     },
     filter: () => {
         var a, i, txtValue;
@@ -46,20 +36,23 @@ const search = {
             txtValue = p.textContent || p.innerText;
             if (txtValue.toUpperCase().indexOf(search.input.value.toUpperCase()) > -1) {
                 a[i].style.display = "";
-            }
-            else {
+            } else {
                 a[i].style.display = "none";
             }
         }
     }
 };
+
 const editor = {
     format: (type) => {
         var sel = window.getSelection();
         var range = sel.getRangeAt(0);
+
         var range0 = range.cloneRange();
+
         range.collapse(true);
         document.execCommand('insertText', false, type);
+
         sel.removeAllRanges();
         sel.addRange(range0);
         range0.collapse(false);
@@ -95,17 +88,18 @@ const editor = {
         if (window.webkitURL != null) {
             // Chrome
             downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-        }
-        else {
+        } else {
             // Firefox
             downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
             downloadLink.onclick = destroyClickedElement;
             downloadLink.style.display = "none";
             document.body.appendChild(downloadLink);
         }
+    
         downloadLink.click();
     }
 };
+
 onkeydown = function (e) {
     if (e.ctrlKey) {
         switch (e.keyCode) {
@@ -124,9 +118,13 @@ onkeydown = function (e) {
         }
     }
 };
+
 search.input.addEventListener('keyup', search.filter);
+
 spotlight.element.addEventListener('click', search.show);
+
 document.getElementById('bold').addEventListener('click', () => editor.format('**'));
+
 const properties = [
     'direction',
     'boxSizing',
@@ -134,15 +132,18 @@ const properties = [
     'height',
     'overflowX',
     'overflowY',
+
     'borderTopWidth',
     'borderRightWidth',
     'borderBottomWidth',
     'borderLeftWidth',
     'borderStyle',
+
     'paddingTop',
     'paddingRight',
     'paddingBottom',
     'paddingLeft',
+
     'fontStyle',
     'fontVariant',
     'fontWeight',
@@ -151,188 +152,229 @@ const properties = [
     'fontSizeAdjust',
     'lineHeight',
     'fontFamily',
+
     'textAlign',
     'textTransform',
     'textIndent',
     'textDecoration',
+
     'letterSpacing',
     'wordSpacing',
+
     'tabSize',
     'MozTabSize',
-];
-const isFirefox = typeof window !== 'undefined' && window['mozInnerScreenX'] != null;
+]
+
+const isFirefox = typeof window !== 'undefined' && window['mozInnerScreenX'] != null
+
 /**
  * @param {HTMLTextAreaElement} element
  * @param {number} position
  */
 function getCaretCoordinates(element, position) {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const style = div.style;
-    const computed = getComputedStyle(element);
-    style.whiteSpace = 'pre-wrap';
-    style.wordWrap = 'break-word';
-    style.position = 'absolute';
-    style.visibility = 'hidden';
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const style = div.style
+    const computed = getComputedStyle(element)
+
+    style.whiteSpace = 'pre-wrap'
+    style.wordWrap = 'break-word'
+    style.position = 'absolute'
+    style.visibility = 'hidden'
+
     properties.forEach(prop => {
-        style[prop] = computed[prop];
-    });
+        style[prop] = computed[prop]
+    })
+
     if (isFirefox) {
         if (element.scrollHeight > parseInt(computed.height))
-            style.overflowY = 'scroll';
+            style.overflowY = 'scroll'
+    } else {
+        style.overflow = 'hidden'
     }
-    else {
-        style.overflow = 'hidden';
-    }
-    div.textContent = element.value.substring(0, position);
-    const span = document.createElement('span');
-    span.textContent = element.value.substring(position) || '.';
-    div.appendChild(span);
+
+    div.textContent = element.value.substring(0, position)
+
+    const span = document.createElement('span')
+    span.textContent = element.value.substring(position) || '.'
+    div.appendChild(span)
+
     const coordinates = {
         top: span.offsetTop + parseInt(computed['borderTopWidth']),
         left: span.offsetLeft + parseInt(computed['borderLeftWidth']),
         // height: parseInt(computed['lineHeight'])
         height: span.offsetHeight
-    };
-    div.remove();
-    return coordinates;
+    }
+
+    div.remove()
+
+    return coordinates
 }
+
 class Mentionify {
     constructor(ref, menuRef, resolveFn, replaceFn, menuItemFn) {
-        this.ref = ref;
-        this.menuRef = menuRef;
-        this.resolveFn = resolveFn;
-        this.replaceFn = replaceFn;
-        this.menuItemFn = menuItemFn;
-        this.options = [];
-        this.makeOptions = this.makeOptions.bind(this);
-        this.closeMenu = this.closeMenu.bind(this);
-        this.selectItem = this.selectItem.bind(this);
-        this.onInput = this.onInput.bind(this);
-        this.onKeyDown = this.onKeyDown.bind(this);
-        this.renderMenu = this.renderMenu.bind(this);
-        this.ref.addEventListener('input', this.onInput);
-        this.ref.addEventListener('keydown', this.onKeyDown);
+        this.ref = ref
+        this.menuRef = menuRef
+        this.resolveFn = resolveFn
+        this.replaceFn = replaceFn
+        this.menuItemFn = menuItemFn
+        this.options = []
+
+        this.makeOptions = this.makeOptions.bind(this)
+        this.closeMenu = this.closeMenu.bind(this)
+        this.selectItem = this.selectItem.bind(this)
+        this.onInput = this.onInput.bind(this)
+        this.onKeyDown = this.onKeyDown.bind(this)
+        this.renderMenu = this.renderMenu.bind(this)
+
+        this.ref.addEventListener('input', this.onInput)
+        this.ref.addEventListener('keydown', this.onKeyDown)
     }
-    makeOptions(query) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const options = yield this.resolveFn(query);
-            if (options.lenght !== 0) {
-                this.options = options;
-                this.renderMenu();
-            }
-            else {
-                this.closeMenu();
-            }
-        });
+
+    async makeOptions(query) {
+        const options = await this.resolveFn(query)
+        if (options.lenght !== 0) {
+            this.options = options
+            this.renderMenu()
+        } else {
+            this.closeMenu()
+        }
     }
+
     closeMenu() {
         setTimeout(() => {
-            this.options = [];
-            this.left = undefined;
-            this.top = undefined;
-            this.triggerIdx = undefined;
-            this.renderMenu();
-        }, 0);
+            this.options = []
+            this.left = undefined
+            this.top = undefined
+            this.triggerIdx = undefined
+            this.renderMenu()
+        }, 0)
     }
+
     selectItem(active) {
         return () => {
-            const preMention = this.ref.value.substr(0, this.triggerIdx);
-            const option = this.options[active];
-            const mention = this.replaceFn(option, this.ref.value[this.triggerIdx]);
-            const postMention = this.ref.value.substr(this.ref.selectionStart);
-            const newValue = `${preMention}${mention}${postMention}`;
-            this.ref.value = newValue;
-            const caretPosition = this.ref.value.length - postMention.length;
-            this.ref.setSelectionRange(caretPosition, caretPosition);
-            this.closeMenu();
-            this.ref.focus();
-        };
+            const preMention = this.ref.value.substr(0, this.triggerIdx)
+            const option = this.options[active]
+            const mention = this.replaceFn(option, this.ref.value[this.triggerIdx])
+            const postMention = this.ref.value.substr(this.ref.selectionStart)
+            const newValue = `${preMention}${mention}${postMention}`
+            this.ref.value = newValue
+            const caretPosition = this.ref.value.length - postMention.length
+            this.ref.setSelectionRange(caretPosition, caretPosition)
+            this.closeMenu()
+            this.ref.focus()
+        }
     }
+
     onInput(ev) {
-        const positionIndex = this.ref.selectionStart;
-        const textBeforeCaret = this.ref.value.slice(0, positionIndex);
-        const tokens = textBeforeCaret.split(/\s/);
-        const lastToken = tokens[tokens.length - 1];
+        const positionIndex = this.ref.selectionStart
+        const textBeforeCaret = this.ref.value.slice(0, positionIndex)
+        const tokens = textBeforeCaret.split(/\s/)
+        const lastToken = tokens[tokens.length - 1]
         const triggerIdx = textBeforeCaret.endsWith(lastToken)
             ? textBeforeCaret.length - lastToken.length
-            : -1;
-        const maybeTrigger = textBeforeCaret[triggerIdx];
-        const keystrokeTriggered = maybeTrigger === '@';
+            : -1
+        const maybeTrigger = textBeforeCaret[triggerIdx]
+        const keystrokeTriggered = maybeTrigger === '@'
+
         if (!keystrokeTriggered) {
-            this.closeMenu();
-            return;
+            this.closeMenu()
+            return
         }
-        const query = textBeforeCaret.slice(triggerIdx + 1);
-        this.makeOptions(query);
-        const coords = getCaretCoordinates(this.ref, positionIndex);
-        const { top, left } = this.ref.getBoundingClientRect();
+
+        const query = textBeforeCaret.slice(triggerIdx + 1)
+        this.makeOptions(query)
+
+        const coords = getCaretCoordinates(this.ref, positionIndex)
+        const { top, left } = this.ref.getBoundingClientRect()
+
         setTimeout(() => {
-            this.active = 0;
-            this.left = window.scrollX + coords.left + left + this.ref.scrollLeft;
-            this.top = window.scrollY + coords.top + top + coords.height - this.ref.scrollTop;
-            this.triggerIdx = triggerIdx;
-            this.renderMenu();
-        }, 0);
+            this.active = 0
+            this.left = window.scrollX + coords.left + left + this.ref.scrollLeft
+            this.top = window.scrollY + coords.top + top + coords.height - this.ref.scrollTop
+            this.triggerIdx = triggerIdx
+            this.renderMenu()
+        }, 0)
     }
+
     onKeyDown(ev) {
-        let keyCaught = false;
+        let keyCaught = false
         if (this.triggerIdx !== undefined) {
             switch (ev.key) {
                 case 'ArrowDown':
-                    this.active = Math.min(this.active + 1, this.options.length - 1);
-                    this.renderMenu();
-                    keyCaught = true;
-                    break;
+                    this.active = Math.min(this.active + 1, this.options.length - 1)
+                    this.renderMenu()
+                    keyCaught = true
+                    break
                 case 'ArrowUp':
-                    this.active = Math.max(this.active - 1, 0);
-                    this.renderMenu();
-                    keyCaught = true;
-                    break;
+                    this.active = Math.max(this.active - 1, 0)
+                    this.renderMenu()
+                    keyCaught = true
+                    break
                 case 'Enter':
                 case 'Tab':
-                    this.selectItem(this.active)();
-                    keyCaught = true;
-                    break;
+                    this.selectItem(this.active)()
+                    keyCaught = true
+                    break
             }
         }
+
         if (keyCaught) {
-            ev.preventDefault();
+            ev.preventDefault()
         }
     }
+
     renderMenu() {
         if (this.top === undefined) {
-            this.menuRef.hidden = true;
-            return;
+            this.menuRef.hidden = true
+            return
         }
-        this.menuRef.style.left = this.left + 'px';
-        this.menuRef.style.top = this.top + 'px';
-        this.menuRef.innerHTML = '';
+
+        this.menuRef.style.left = this.left + 'px'
+        this.menuRef.style.top = this.top + 'px'
+        this.menuRef.innerHTML = ''
+
         this.options.forEach((option, idx) => {
-            this.menuRef.appendChild(this.menuItemFn(option, this.selectItem(idx), this.active === idx));
-        });
-        this.menuRef.hidden = false;
+            this.menuRef.appendChild(this.menuItemFn(
+                option,
+                this.selectItem(idx),
+                this.active === idx))
+        })
+
+        this.menuRef.hidden = false
     }
 }
+
 const users = [
     { username: 'davidbrzy' },
     { username: 'jakubklapka' },
     { username: 'johndoe' },
-];
+]
+
 const resolveFn = prefix => prefix === ''
     ? users
-    : users.filter(user => user.username.startsWith(prefix));
-const replaceFn = (user, trigger) => `${trigger}${user.username} `;
+    : users.filter(user => user.username.startsWith(prefix))
+
+const replaceFn = (user, trigger) => `${trigger}${user.username} `
+
 const menuItemFn = (user, setItem, selected) => {
-    const div = document.createElement('div');
-    div.setAttribute('role', 'option');
-    div.className = 'menu-item';
+    const div = document.createElement('div')
+    div.setAttribute('role', 'option')
+    div.className = 'menu-item'
     if (selected) {
-        div.classList.add('selected');
-        div.setAttribute('aria-selected', '');
+        div.classList.add('selected')
+        div.setAttribute('aria-selected', '')
     }
-    div.textContent = user.username;
-    div.onclick = setItem;
-    return div;
-};
-new Mentionify(document.getElementById('textarea'), document.getElementById('menu'), resolveFn, replaceFn, menuItemFn);
+    div.textContent = user.username
+    div.onclick = setItem
+    return div
+}
+
+new Mentionify(
+    document.getElementById('textarea'),
+    document.getElementById('menu'),
+    resolveFn,
+    replaceFn,
+    menuItemFn
+)
